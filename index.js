4d9f9070475beda9dc8d09ce653739d62d0d48b2
Emmanuel <emmanuel.k.kipchumba@gmail.com>
2024-06-26 01:00:14 -0400

    arithmetric lab

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+// multiply
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2; // 31 * 2 = 62
+
+// random
+const random = Math.floor(Math.random() * 100) + 1; 
+// Generates a random integer greater than 0
+
+//mod
+let num3 = 10;
+let num4 = 6;
+const mod = num3 % num4; // 10 % 6 = 4
+
+//max
+const max = Math.max(1, 2, 3, 4, 20, 5); 
+// 20 be the highest number in the set
